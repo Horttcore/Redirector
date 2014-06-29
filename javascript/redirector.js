d2b7c2b4d4c11379edf6f92945f141d70eda12ff
Ralf Hortt <me@horttcore.de>
2014-06-29 21:25:18 +0200

    Version 3.0
    * Added: Search post object for post redirect type
    * Added: Redirect preview
    * Added action: `redirector-update`
    * Added action: `redirector-modal-search-begin`
    * Added action: `redirector-modal-search-end`
    * Added filter: `redirector-recent-posts`
    * Added filter: `redirector-redirect-url`
    * Added filter: `redirector-status-code`
    * Added filter: `redirector-meta`
    * Enhancement: New Redirector UI
    * Removed Filter: `redirector_redirect`
    * Removed Filter: `redirector_status`
    * Removed Filter: `redirector_types`
    
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- javascript/redirector.js
+++ javascript/redirector.js
@@ -1,73 +1,180 @@
-jQuery(function(){
+jQuery(document).ready(function(){
 
+	var Plugin = {
 
+		init:function(){
 
-	/**
-	 *
-	 * Cache elements
-	 *
-	 */
-	var settings_page = jQuery('#redirect_settings_page');
-	var settings_url = jQuery('#redirect_settings_url');
-	var treeSelect = jQuery('#redirector_tree');
-	var urlInput = jQuery('#redirector_url');
-	var redirectPage = jQuery('#redirect_page');
-	var redirectUrl = jQuery('#redirect_url');
+			// Cache
+			Plugin.body = jQuery('body');
+			Plugin.radioButtons = jQuery('input[name="redirect-type"]');
+			Plugin.radioOptions = jQuery('.redirector-redirect-type > div');
 
+			Plugin.radioButtonChecked = false;
+			Plugin.radioOptionActive = false;
 
+			Plugin.redirectIDButton = jQuery('#redirector-set-post-id');
+			Plugin.redirectID = jQuery('#redirector-post-id');
+			Plugin.redirectIDPreview = jQuery('#redirector-post-id-preview');
 
-	/**
-	 *
-	 * Update Redirector Metabox
-	 *
-	 */
-	function update_redirector_metabox() {
-		settings_url.hide();
-		settings_page.hide();
+			Plugin.url = jQuery('input[name="redirector-url"]');
+			Plugin.urlPreview = jQuery('#redirector-url-preview');
 
-		obj = jQuery('input[name=redirect_type]:checked');
+			Plugin.thickboxWindow = false;
+			Plugin.thickboxTitle = false;
+			Plugin.thickboxContent = false;
 
-		if ( 'page' == obj.data('type') ) {
-			settings_page.show();
-		} else if ( 'url' == obj.data('type') ) {
-			settings_url.show();
+			// Bindings
+			Plugin.bindings();
+		},
+
+		bindings:function(){
+
+			// Toggle options
+			Plugin.toggleOptions();
+			Plugin.radioButtons.change(function(){
+
+				Plugin.toggleOptions( jQuery(this ) );
+
+			});
+
+			// Open thickbox
+			Plugin.redirectIDButton.click(function(e){
+				e.preventDefault();
+				Plugin.resetSearch();
+				setTimeout( function(){
+					Plugin.setThickboxSize();
+				}, 500 );
+			});
+
+			// Select redirector post
+			Plugin.body.on( 'click', '.select-redirector-post-id', function(e){
+				e.preventDefault();
+				Plugin.redirectID.val( jQuery(this).data('id') );
+				Plugin.redirectIDPreview.html( jQuery(this).parents('tr:first').find('.item-title a').clone() )
+				jQuery('.tb-close-icon').trigger('click');
+				Plugin.resetSearch();
+			});
+
+			// URL preview
+			Plugin.url.keyup(function(){
+				Plugin.urlPreview.text( Plugin.url.val() )
+				Plugin.urlPreview.prop( 'href', Plugin.url.val() )
+			});
+
+			// Search Post
+			Plugin.body.on( 'click', '#redirector-search-post', function(e){
+				e.preventDefault();
+				Plugin.searchPost();
+			});
+
+			Plugin.body.on( 'keyup', '#redirector-search', function(e){
+
+				if ( 13 !== e.which )
+					return;
+
+				e.preventDefault();
+				Plugin.searchPost();
+
+			}).on( 'change', '#redirector-search', function(e){
+
+				if ( '' == jQuery(this).val() )
+					Plugin.resetSearch();
+
+			});
+
+		},
+
+		clearOptions:function( obj ) {
+
+			if ( 'url' != Plugin.radioButtonChecked.val() ) {
+				Plugin.url.val('');
+				Plugin.urlPreview.prop('href','').text('');
+			}
+
+			if ( 'post' != Plugin.radioButtonChecked.val() ) {
+				Plugin.redirectID.val('');
+				Plugin.redirectIDPreview.html('');
+			}
+
+		},
+
+		resetSearch:function(){
+
+			var search = jQuery('#redirector-search'),
+				recent = jQuery('#redirector-recent-posts'),
+				result = jQuery('#redirector-search-result');
+
+			search.val('');
+			result.html('');
+			recent.show();
+
+		},
+
+		searchPost:function() {
+
+			var search = jQuery('#redirector-search'),
+				button = jQuery('#redirector-search-post'),
+				recent = jQuery('#redirector-recent-posts'),
+				result = jQuery('#redirector-search-result')
+				spinner = false;
+
+			if ( '' != search.val() ) {
+
+				// Add spinner
+				button.after('<span class="spinner" id="redirector-spinner"></span>');
+
+				// Hide recent posts
+				recent.hide();
+
+				// Search
+				jQuery.post(ajaxurl, {action: 'redirector-search-posts', search: search.val(), nonce: redirector.searchNonce }, function(response){
+
+					// Inject search result
+					result.html( response.output );
+
+					// Remove spinner
+					button.next().remove();
+
+				}, 'json' );
+
+			} else {
+
+				Plugin.resetSearch();
+
+			}
+
+		},
+
+		setThickboxSize:function(){
+
+			Plugin.thickboxWindow = jQuery('#TB_window');
+			Plugin.thickboxTitle = jQuery('#TB_title');
+			Plugin.thickboxContent = jQuery('#TB_ajaxContent');
+
+			var css = {
+				width: parseInt( Plugin.thickboxWindow.width() ) - parseInt( Plugin.thickboxContent.css('paddingLeft') ) - parseInt( Plugin.thickboxContent.css('paddingRight') ),
+				height: parseInt( Plugin.thickboxWindow.height() ) - parseInt( Plugin.thickboxTitle.height() ) - parseInt( Plugin.thickboxContent.css('paddingBottom') ) - parseInt( Plugin.thickboxContent.css('paddingBottom') ),
+				overflow: 'scroll'
+			}
+
+			Plugin.thickboxContent.css( css );
+
+		},
+
+		toggleOptions:function( obj ) {
+
+			Plugin.radioButtonChecked = Plugin.radioButtons.filter(':checked');
+			Plugin.radioOptionActive = Plugin.radioButtonChecked.parents('.redirector-redirect-type').find('> div');
+
+			Plugin.clearOptions( obj );
+			Plugin.radioOptions.hide();
+			Plugin.radioOptionActive.show();
+
 		}
+
+
 	}
 
+	Plugin.init();
 
-
-	/**
-	 *
-	 * Update fields
-	 *
-	 */
-	treeSelect.on('change', function(e){
-		obj = jQuery(this);
-		redirectPage.val(obj.val());
-	});
-
-	urlInput.on('change blur', function(e){
-		obj = jQuery(this);
-		redirectUrl.val(obj.val());
-	});
-
-
-
-	/**
-	 *
-	 * Show Settings
-	 *
-	 */
-	jQuery('input[name=redirect_type]').click(function(e){
-		update_redirector_metabox();
-	});
-
-
-
-	/**
-	 *
-	 * Run on DOM READY
-	 *
-	 */
-	update_redirector_metabox();
 });
